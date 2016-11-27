69f7335f96198273c1f61c5ee85cbbe495c0a4fb
hsorensen <henrik.b.sorensen@gmail.com>
2016-11-26 18:26:29 -0800

    Clean up source code.

--- antlr4ide.ui/src/main/java/com/github/jknack/antlr4ide/ui/decorator/Antlr4SuffixDecorator.java
+++ antlr4ide.ui/src/main/java/com/github/jknack/antlr4ide/ui/decorator/Antlr4SuffixDecorator.java
@@ -6,23 +6,20 @@ import org.eclipse.jface.viewers.ILabelProviderListener;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IResource;
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Antlr4SuffixDecorator implements ILightweightLabelDecorator
 {
-   // read first line from file
-   // the ANTLR4 generated java files has first line indicating origin
+   // For the ANTLR4 generated java files has first line indicating origin
    //    >// Generated from testissue.g4 by ANTLR 4.5.1<
-   // the tokens files have no indication of origin except for the filename of course.
+   // For the generated tokens files use the filename itself as indication of origin.
 	
    static final String[] checkSuffixes={"Lexer.tokens","Parser.tokens",".tokens" };
    static final int[]    checkSuffixesLen={checkSuffixes[0].length(),checkSuffixes[1].length(),checkSuffixes[2].length()};
 
    public void decorate(Object element, IDecoration decoration) {
-	   // Note the plugin.xml specify which types are passed to this decorator
+	   // Note: the plugin.xml specify which types are passed through element to this decorator
 	   IResource resource=(IResource)element;
-	   String resourceName = resource.getName();
 	   
 //	   System.out.println(" Antlr4SuffixDecorator decorate Object>"+element.getClass()+"<"
 //	   		+ ">"+resource.getName()+"<"
@@ -32,7 +29,9 @@ public class Antlr4SuffixDecorator implements ILightweightLabelDecorator
 	   
 	   if(resource.getType()!=IResource.FILE) return;
 	   if(!resource.isDerived()) return;
-	   
+
+	   String resourceName = resource.getName();
+
 	   if(resourceName.endsWith(".java")) { // what about other target types?
 		   // read first line
 		    BufferedReader in=null;
@@ -59,31 +58,6 @@ public class Antlr4SuffixDecorator implements ILightweightLabelDecorator
    }
    
 
-/*
-    * invoked by eclipse when selecting decorator in preferences or by start
-    */
-   public void addListener(ILabelProviderListener listener) { 
-   }
-   
-   /*
-    * invoked before dispose, when unselecting the decorator in preferences
-    */
-   public void removeListener(ILabelProviderListener listener) { 
-   }
-   
-   /*
-    * invoked after removeListener, when unselecting the decorator in preferences
-    */
-   public void dispose() { 
-//	   System.out.println(" Antlr4SuffixDecorator dispose"); 
-   }
-   
-   public boolean isLabelProperty(Object element, String property) { 
-//	   System.out.println(" Antlr4SuffixDecorator isLabelProperty>"+element.getClass()+"<>"+property+"<");
-	   return false; 
-   }
-   
-   
    private String asDecoratorSuffix(String fileName) {
 	   return " ["+fileName+"]";
    }
@@ -91,5 +65,11 @@ public class Antlr4SuffixDecorator implements ILightweightLabelDecorator
    private String asDecoratorSuffix(String resourceName, int resourceSuffixLen, String ext) {
 	   return asDecoratorSuffix(resourceName.substring(0,resourceName.length()-resourceSuffixLen)+ext);
    }
+   
+   public void addListener(ILabelProviderListener listener) {  }
+   public void removeListener(ILabelProviderListener listener) {  }
+   public void dispose() {  }
+   public boolean isLabelProperty(Object element, String property) { return false; }
+   
 
 }
